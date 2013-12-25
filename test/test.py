fb9f86cf45bb0db49da79e8b49fa7fee2b542c40
Anton Panasenko <anton.panasenko@gmail.com>
2013-12-25 18:42:14 +0200

    Added testing environment

--- test/test.py
+++ test/test.py
@@ -1,11 +1,58 @@
 import os
-import pymysql
+
 import requests
+from qubell.api.private.testing import environment, instance, values
 
 from test_runner import BaseComponentTestCase
-from qubell.api.private.testing import instance, workflow, values
 
 
+@environment({
+    "default": {},
+    "AmazonEC2_CentOS_63": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-eb6b0182"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "root"
+        }]
+    },
+    "AmazonEC2_CentOS_53": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-beda31d7"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "root"
+        }]
+    },
+    "AmazonEC2_Ubuntu_1204": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-d0f89fb9"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "ubuntu"
+        }]
+    },
+    "AmazonEC2_Ubuntu_1004": {
+        "policies": [{
+            "action": "provisionVms",
+            "parameter": "imageId",
+            "value": "us-east-1/ami-0fac7566"
+        }, {
+            "action": "provisionVms",
+            "parameter": "vmIdentity",
+            "value": "ubuntu"
+        }]
+    }
+})
 class PetClinicComponentTestCase(BaseComponentTestCase):
     name = "component-petclinic"
     apps = [{
